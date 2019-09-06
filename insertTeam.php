cc67c7556945d9cae033741fb109d847eff0e5d5
Yuvaraj Jayavel <jyuvaraj03@gmail.com>
2019-09-06 19:50:06 +0530

    Finished registration using php

---
+++ insertTeam.php
@@ -0,0 +1,48 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "svcehost_yah";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$teams_sql = "INSERT INTO teams (team_name, participant_count) VALUES (?, ?);";
+$participants_sql = "INSERT INTO participants (team_name, participant_name, college, email, phone, github_url) VALUES (?,?,?,?,?,?);";
+$abstracts_sql = "INSERT INTO abstracts (team_name, domain, abstract) VALUES (?, ?, ?)";
+
+$teams_stmt = $conn->prepare($teams_sql);
+$participants_stmt = $conn->prepare($participants_sql);
+$abstracts_stmt = $conn->prepare($abstracts_sql);
+
+$teams_stmt->bind_param("si", $_POST['team_name'], $_POST['participant_count']);
+$teams_stmt->execute();
+
+$participants_stmt->bind_param("ssssss", $_POST['team_name'], $_POST['name_0'], $_POST['college_0'], $_POST['email_0'], $_POST['phone_0'], $_POST['github_url_0']);
+$participants_stmt->execute();
+if ($_POST['participant_count'] > 1) {
+	$participants_stmt->bind_param("ssssss", $_POST['team_name'], $_POST['name_1'], $_POST['college_1'], $_POST['email_1'], $_POST['phone_1'], $_POST['github_url_1']);
+	$participants_stmt->execute();
+} 
+if ($_POST['participant_count'] > 2) {
+	$participants_stmt->bind_param("ssssss", $_POST['team_name'], $_POST['name_2'], $_POST['college_2'], $_POST['email_2'], $_POST['phone_2'], $_POST['github_url_2']);
+	$participants_stmt->execute();
+} 
+if ($_POST['participant_count'] > 3) {
+	$participants_stmt->bind_param("ssssss", $_POST['team_name'], $_POST['name_3'], $_POST['college_3'], $_POST['email_3'], $_POST['phone_3'], $_POST['github_url_3']);
+	$participants_stmt->execute();
+}
+if ($_POST['participant_count'] > 4) {
+	$participants_stmt->bind_param("ssssss", $_POST['team_name'], $_POST['name_4'], $_POST['college_4'], $_POST['email_4'], $_POST['phone_4'], $_POST['github_url_4']);
+	$participants_stmt->execute();
+}
+
+$abstracts_stmt->bind_param("sss", $_POST['team_name'], $_POST['domain'], $_POST['abstract']);
+$abstracts_stmt->execute();
+
+$conn->close();
+?>
---
+++ register.js
@@ -0,0 +1,89 @@
+// Validation to be done 
+
+var participant_count;
+$('#tab-0 .next').click(function (event) {
+	event.preventDefault();
+	participant_count = $('#tab-0 select').val();
+	$('#tab-0').css("display", 'none');
+	$('#tab-1').css('display', 'flex');
+});
+
+$('#tab-1 .next').click(function (event) {
+	event.preventDefault();
+	$('#tab-1').css("display", 'none');
+	if (participant_count > 1) {
+		$('#tab-2').css('display', 'flex');
+	} else {
+		$('#tab-5').css('display', 'flex');
+	}
+});
+
+$('#tab-2 .next').click(function (event) {
+	event.preventDefault();
+	$('#tab-2').css("display", 'none');
+	if (participant_count > 2) {
+		$('#tab-3').css('display', 'flex');
+	} else {
+		$('#tab-5').css('display', 'flex');
+	}
+});
+
+$('#tab-3 .next').click(function (event) {
+	event.preventDefault();
+	$('#tab-3').css("display", 'none');
+	if (participant_count > 3) {
+		$('#tab-4').css('display', 'flex');
+	} else {
+		$('#tab-5').css('display', 'flex');
+	}
+});
+
+$('#tab-4 .next').click(function (event) {
+	event.preventDefault();
+	$('#tab-4').css("display", 'none');
+	$('#tab-5').css('display', 'flex');
+});
+
+$('#tab-5 .next').click(function (event) {
+	event.preventDefault();
+	// Do validation
+	$('form').submit();
+})
+
+$('#tab-1 .back').click(function(event) {
+	event.preventDefault();
+	$('#tab-1').css("display", 'none');
+	$('#tab-0').css("display", "flex");
+});
+
+$('#tab-2 .back').click(function(event) {
+	event.preventDefault();
+	$('#tab-2').css("display", 'none');
+	$('#tab-1').css("display", "flex");
+});
+
+$('#tab-3 .back').click(function(event) {
+	event.preventDefault();
+	$('#tab-3').css("display", 'none');
+	$('#tab-2').css("display", "flex");
+});
+
+$('#tab-4 .back').click(function(event) {
+	event.preventDefault();
+	$('#tab-4').css("display", 'none');
+	$('#tab-3').css("display", "flex");
+});
+
+$('#tab-5 .back').click(function(event) {
+	event.preventDefault();
+	$('#tab-5').css('display', 'none');
+	if (participant_count == 4) {
+		$('#tab-4').css('display', 'flex');
+	} else if (participant_count == 3) {
+		$('#tab-3').css('display', 'flex');
+	} else if (participant_count == 2) {
+		$('#tab-2').css('display', 'flex');
+	} else if (participant_count == 1) {
+		$('#tab-1').css('display', 'flex');
+	}
+});
